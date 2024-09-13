ad90a796e1af7c4944e05fb3faca609384c8f19c
LightDestory <apb231@gmail.com>
2024-09-13 09:14:06 +0200

    chore: add comment to (some) models

--- socket/events.go
+++ socket/events.go
@@ -1,17 +1,31 @@
 package socket
 
+// Event is a type representing an event that is emitted to the console.
 const (
-	EventLog                = "console-sim"
-	EventError              = "console-error"
-	EventDev                = "log-dev"
-	EventGw                 = "log-gw"
-	EventToggleStateDevice  = "toggleState-dev"
+	// EventLog represents the event type for console simulation logs.
+	EventLog = "console-sim"
+	// EventError represents an error log event that is emitted to the console.
+	EventError = "console-error"
+	// EventDev is a constant representing the "log-dev" event type used for device logging.
+	EventDev = "log-dev"
+	// EventGw is a string constant representing the "log-gw" event type, typically used for logging gateway-specific messages.
+	EventGw = "log-gw"
+	// EventToggleStateDevice represents an event used to toggle the state of a device.
+	EventToggleStateDevice = "toggleState-dev"
+	// EventToggleStateGateway is a constant representing the event for toggling the state of a gateway.
 	EventToggleStateGateway = "toggleState-gw"
-	EventSaveStatus         = "save-status"
-	EventMacCommand         = "send-MACCommand"
-	EventResponseCommand    = "response-command"
-	EventChangePayload      = "change-payload"
-	EventSendUplink         = "send-uplink"
-	EventChangeLocation     = "change-location"
-	EventGetParameters      = "get-regional-parameters"
+	// EventSaveStatus represents the event name used to indicate the saving of a status in the system.
+	EventSaveStatus = "save-status"
+	// EventMacCommand represents the event identifier for sending a MAC command to a device.
+	EventMacCommand = "send-MACCommand"
+	// EventResponseCommand is used to indicate a response to a command issued to a device or gateway.
+	EventResponseCommand = "response-command"
+	// EventChangePayload is an event type for signaling a payload change in the system.
+	EventChangePayload = "change-payload"
+	// EventSendUplink represents the event for sending an uplink message in the system.
+	EventSendUplink = "send-uplink"
+	// EventChangeLocation is the constant for the "change-location" event.
+	EventChangeLocation = "change-location"
+	// EventGetParameters is the event name used for requesting regional parameters in the simulation environment.
+	EventGetParameters = "get-regional-parameters"
 )
--- socket/models.go
+++ socket/models.go
@@ -2,35 +2,40 @@ package socket
 
 import "github.com/brocaar/lorawan"
 
+// ConsoleLog represents a log entry with a name and a message.
 type ConsoleLog struct {
-	Name string `json:"name"`
-	Msg  string `json:"message"`
+	Name string `json:"name"`    // Name specifies the source or identifier of the log entry.
+	Msg  string `json:"message"` // Msg contains the actual log message.
 }
 
+// NewStatusDev represents the status of a device in the network, including identifiers and counters.
 type NewStatusDev struct {
-	DevEUI   lorawan.EUI64   `json:"devEUI"`
-	DevAddr  lorawan.DevAddr `json:"devAddr"`
-	NwkSKey  string          `json:"nwkSKey"`
-	AppSKey  string          `json:"appSKey"`
-	FCntDown uint32          `json:"fcntDown"`
-	FCnt     uint32          `json:"fcnt"`
+	DevEUI   lorawan.EUI64   `json:"devEUI"`   // DevEUI is the unique identifier of the device.
+	DevAddr  lorawan.DevAddr `json:"devAddr"`  // DevAddr is the device address.
+	NwkSKey  string          `json:"nwkSKey"`  // NwkSKey is the network session key.
+	AppSKey  string          `json:"appSKey"`  // AppSKey is the application session key.
+	FCntDown uint32          `json:"fcntDown"` // FCntDown is the downlink frame counter.
+	FCnt     uint32          `json:"fcnt"`     // FCnt is the uplink frame counter.
 }
 
+// NewPayload represents a structure for handling payload changes with ID, message type, and payload data.
 type NewPayload struct {
-	Id      int    `json:"id"`
-	MType   string `json:"mtype"`
-	Payload string `json:"payload"`
+	Id      int    `json:"id"`      // Id is the unique identifier of the payload.
+	MType   string `json:"mtype"`   // MType is the message type.
+	Payload string `json:"payload"` // Payload is the actual payload data.
 }
 
+// NewLocation represents the geographical location of a device.
 type NewLocation struct {
-	Id        int     `json:"id"`
-	Latitude  float64 `json:"latitude"`
-	Longitude float64 `json:"longitude"`
-	Altitude  int32   `json:"altitude"`
+	Id        int     `json:"id"`        // Id is the unique identifier of the location.
+	Latitude  float64 `json:"latitude"`  // Latitude is the geographical latitude.
+	Longitude float64 `json:"longitude"` // Longitude is the geographical longitude.
+	Altitude  int32   `json:"altitude"`  // Altitude is the height above sea level.
 }
 
+// MacCommand represents a MAC command to be sent to a device in the network.
 type MacCommand struct {
-	Id          int    `json:"id"`
-	CID         string `json:"cid"`
-	Periodicity uint8  `json:"periodicity"`
+	Id          int    `json:"id"`          // Id is the unique identifier of the MAC command.
+	CID         string `json:"cid"`         // CID is the command identifier.
+	Periodicity uint8  `json:"periodicity"` // Periodicity is the interval at which the command is sent.
 }
